7d0bda6c743d20cfd2b688fadf3854eef273b5a0
KushibikiMashu <kushibiki3413@gmail.com>
2018-07-19 23:54:45 +0900

    [master]アドレスが連番の場合に対応

--- index.js
+++ index.js
@@ -1,35 +1,37 @@
 const https = require('https')
 const fs = require('fs')
 
-// サンプル画像
-var url = 'https://1.bp.blogspot.com/-wXNSXGxGLcY/WzC-Nrk2AgI/AAAAAAABM_k/S-2MEmlmO40My55xP42pJ3PIoEtBACLcBGAs/s800/bucket_relay_nimots.png'
-
 // 日付を取得
 var today = new Date();
 var datetime = today.getFullYear() + '_' + (today.getMonth() + 1) + '_' + today.getDate()
+var i = 1
 
-// ダウンロード開始
-var req = https.get(url, function (res) {
+while (true) {
+  var num = '00' + i
+  num = num.slice(-3)
 
-  var i = 1
+  // サンプル画像
+  var url = 'https://1.bp.blogspot.com/-wXNSXGxGLcY/WzC-Nrk2AgI/AAAAAAABM_k/S-2MEmlmO40My55xP42pJ3PIoEtBACLcBGAs/s800/' + num + '.png'
 
-  while (res.statusCode === '200') {
-    var num = '00' + i
-    num = num.slice(-3)
+  // ダウンロード開始
+  var req = https.get(url, function (res) {
+    if (res.statusCode !== 200) {
+      break
+    }
 
     // ファイルの出力場所と名前
     var filename = fs.createWriteStream('./files/' + datetime + '_' + num + '.png')
     res.pipe(filename)
 
     res.on('end', function () {
-        filename.close()
-      })
+      filename.close()
+    })
+  })
 
-      ++i
-  }
-})
+  req.on('error', function (err) {
+      console.log('Error: ', err)
+      return
+    })
 
-req.on('error', function (err) {
-  console.log('Error: ', err)
-  return
-})
+    ++i
+}
