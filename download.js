491959ec187fcdb61b4d967e1ff4da5dd3c8b12b
KushibikiMashu <kushibiki3413@gmail.com>
2018-07-20 20:54:47 +0900

    [master]ファイルのダウンロード

---
+++ download.js
@@ -0,0 +1,214 @@
+// ここの値を書き換えてください
+const dir = ''
+const baseUrl = '' + dir
+
+const url_1 = baseUrl + '001.mp4'
+const url_2 = baseUrl + '002.mp4'
+const url_3 = baseUrl + '003.mp4'
+const url_4 = baseUrl + '004.mp4'
+const url_5 = baseUrl + '005.mp4'
+const url_6 = baseUrl + '006.mp4'
+const url_7 = baseUrl + '007.mp4'
+const url_8 = baseUrl + '008.mp4'
+const url_9 = baseUrl + '009.mp4'
+const url_10 = baseUrl + '010.mp4'
+
+const http = require('http')
+const fs = require('fs')
+
+// 今日の日付を取得
+const today = new Date();
+const month = '0' + (today.getMonth() + 1)
+const datetime = today.getFullYear() + '_' + month.slice(-2) + '_' + today.getDate()
+
+// ディレクトリの読み取り
+const readDir = fs.readdir('.', function (err, files) {
+    if (err) {
+        console.log('ディレクトリの読み込みに失敗しました')
+        console.log('error: ' + err)
+    } else {
+        console.log('ディレクトリを読み込みました')
+        checkDir(files)
+    }
+})
+
+// フォルダの再作成を防止する
+const checkDir = function (files) {
+    if (files.indexOf(datetime) == -1) {
+        fs.mkdir(datetime, function (err) {
+            if (err) {
+                console.log('フォルダの作成に失敗しました')
+                console.log('error: ' + err)
+            } else {
+                console.log('フォルダの作成に成功しました')
+            }
+        })
+    } else {
+        console.log('フォルダは既に存在しています')
+    }
+}
+
+// ダウンロードの準備
+var i = 0
+
+// ファイル名001
+var req = http.get(url_1, function (res) {
+    console.log(url_1)
+        ++i
+    var filename = fs.createWriteStream(datetime + '/00' + i + '.mp4')
+    res.pipe(filename)
+
+    res.on('end', function () {
+        filename.close()
+    })
+})
+
+req.on('error', function (err) {
+    console.log('Error: ', err)
+    return
+})
+
+// ファイル名002
+var req_2 = http.get(url_2, function (res) {
+    console.log(url_2)
+        ++i
+    var filename = fs.createWriteStream(datetime + '/00' + i + '.mp4')
+    res.pipe(filename)
+
+    res.on('end', function () {
+        filename.close()
+    })
+})
+
+req_2.on('error', function (err) {
+    console.log('Error: ', err)
+    return
+})
+
+// ファイル名003
+var req = http.get(url_3, function (res) {
+    console.log(url_3)
+        ++i
+    var filename = fs.createWriteStream(datetime + '/00' + i + '.mp4')
+    res.pipe(filename)
+
+    res.on('end', function () {
+        filename.close()
+    })
+})
+
+req.on('error', function (err) {
+    console.log('Error: ', err)
+    return
+})
+
+// ファイル名004
+var req = http.get(url_4, function (res) {
+    ++i
+    var filename = fs.createWriteStream(datetime + '/00' + i + '.mp4')
+    res.pipe(filename)
+
+    res.on('end', function () {
+        filename.close()
+    })
+})
+
+req.on('error', function (err) {
+    console.log('Error: ', err)
+    return
+})
+
+// ファイル名005
+var req = http.get(url_5, function (res) {
+    ++i
+    var filename = fs.createWriteStream(datetime + '/00' + i + '.mp4')
+    res.pipe(filename)
+
+    res.on('end', function () {
+        filename.close()
+    })
+})
+
+req.on('error', function (err) {
+    console.log('Error: ', err)
+    return
+})
+
+// ファイル名006
+var req = http.get(url_6, function (res) {
+    ++i
+    var filename = fs.createWriteStream(datetime + '/00' + i + '.mp4')
+    res.pipe(filename)
+
+    res.on('end', function () {
+        filename.close()
+    })
+})
+
+req.on('error', function (err) {
+    console.log('Error: ', err)
+    return
+})
+// ファイル名007
+var req = http.get(url_7, function (res) {
+    ++i
+    var filename = fs.createWriteStream(datetime + '/00' + i + '.mp4')
+    res.pipe(filename)
+
+    res.on('end', function () {
+        filename.close()
+    })
+})
+
+req.on('error', function (err) {
+    console.log('Error: ', err)
+    return
+})
+
+// ファイル名008
+var req = http.get(url_8, function (res) {
+    ++i
+    var filename = fs.createWriteStream(datetime + '/00' + i + '.mp4')
+    res.pipe(filename)
+
+    res.on('end', function () {
+        filename.close()
+    })
+})
+
+req.on('error', function (err) {
+    console.log('Error: ', err)
+    return
+})
+
+// ファイル名009
+var req = http.get(url_9, function (res) {
+    ++i
+    var filename = fs.createWriteStream(datetime + '/00' + i + '.mp4')
+    res.pipe(filename)
+
+    res.on('end', function () {
+        filename.close()
+    })
+})
+
+req.on('error', function (err) {
+    console.log('Error: ', err)
+    return
+})
+
+// ファイル名010
+var req = http.get(url_10, function (res) {
+    ++i
+    var filename = fs.createWriteStream(datetime + '/0' + i + '.mp4')
+    res.pipe(filename)
+
+    res.on('end', function () {
+        filename.close()
+    })
+})
+
+req.on('error', function (err) {
+    console.log('Error: ', err)
+    return
+})
--- index.js
+++ index.js
@@ -1,26 +1,21 @@
-const https = require('https')
+const http = require('http')
 const fs = require('fs')
 
 // 日付を取得
-var today = new Date();
-var datetime = today.getFullYear() + '_' + (today.getMonth() + 1) + '_' + today.getDate()
+const today = new Date();
+const datetime = today.getFullYear() + '_' + (today.getMonth() + 1) + '_' + today.getDate()
 var i = 1
 
-while (true) {
+
+while (i < 4) {
   var num = '00' + i
   num = num.slice(-3)
 
   // サンプル画像
-  var url = 'https://1.bp.blogspot.com/-wXNSXGxGLcY/WzC-Nrk2AgI/AAAAAAABM_k/S-2MEmlmO40My55xP42pJ3PIoEtBACLcBGAs/s800/' + num + '.png'
+  var url = 'http://127.0.0.1:3000/' + num + '.mp4'
 
-  // ダウンロード開始
-  var req = https.get(url, function (res) {
-    if (res.statusCode !== 200) {
-      break
-    }
-
-    // ファイルの出力場所と名前
-    var filename = fs.createWriteStream('./files/' + datetime + '_' + num + '.png')
+  var req = http.get(url, function (res) {
+    var filename = fs.createWriteStream('writeFiles/' + num + '.mp4')
     res.pipe(filename)
 
     res.on('end', function () {
@@ -28,10 +23,35 @@ while (true) {
     })
   })
 
-  req.on('error', function (err) {
-      console.log('Error: ', err)
-      return
-    })
-
+  console.log('writeFiles/' + num + '.mp4')
     ++i
 }
+
+req.on('error', function (err) {
+  console.log('Error: ', err)
+  return
+})
+
+
+
+// // ダウンロード開始
+// var req = https.get(url, function (res) {
+//   // if (res.statusCode !== 200) {
+//   // }
+
+//   // ファイルの出力場所と名前
+//   var filename = fs.createWriteStream('./files/' + datetime + '_' + num + '.png')
+//   res.pipe(filename)
+
+//   res.on('end', function () {
+//     filename.close()
+//   })
+// })
+
+// req.on('error', function (err) {
+//     console.log('Error: ', err)
+//     return
+//   })
+
+//   ++i
+// // }
